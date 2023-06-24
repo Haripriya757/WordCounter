516f628a1a90f8bca80ca5dcddb7221e3b1761a8
unknown <hpriya637@gmail.com>
2023-06-24 08:52:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [text, setText] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    const words = text.split(' ');
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+  }, [text]);
+
+  return (
+    <div className='container'>
+      <h2>Word Counter</h2>
+      <div>
+        <textarea
+          value={text}
+          onChange={changeHandler}
+          placeholder='Type in the text box to count the words...'
+        ></textarea>
+
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
